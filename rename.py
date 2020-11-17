5a874118f76604439332fa14150992d8bce4ee42
Hsu1685 <68535309+Hsu1685@users.noreply.github.com>
2020-11-17 14:35:58 +0800

    更新檔名由日期加上流水號

--- rename.py
+++ rename.py
@@ -1,5 +1,8 @@
 import os
-path = 'train_images/'
+import datetime
+# 得到今天的時間
+today = datetime.date.today()
+path = 'train_images_2 - コピー/'
 # 獲取該目錄下所有檔案，存入列表中
 f = os.listdir(path)
 print(len(f))
@@ -12,8 +15,8 @@ for i in f:
     # 設定舊檔名（就是路徑+檔名）
     oldname = f[n]
 
-    # 設定新檔名
-    newname = str(n+1) + '.jpg'
+    # 設定新檔名(格式: 今天的時間_流水號)
+    newname = str(today) + '_' + str(n+1) + '.jpg'
     # 用os模組中的rename方法對檔案改名
     os.rename(path+oldname, path+newname)
     print(oldname, '======>', newname)
